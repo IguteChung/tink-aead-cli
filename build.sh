64eaa626e34ec5601e1bde69b9f6022b9ab3d237
frank chung <frank310751@gmail.com>
2022-10-31 15:50:37 +0800

    add zip release

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ for GOOS in ${ALL_GOOS[@]}; do
   for GOARCH in ${ALL_GOARCH[@]}; do
     name="${GOOS}-${GOARCH}-${VERSION}"
     GOOS=${GOOS} GOARCH=${GOARCH} go build -o build/${name}/tink-aead-cli main.go
+    zip build/${name}.zip build/${name}/
   done
 done
